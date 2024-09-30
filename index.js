0b74dcb85397227a362e7b605baa587f181b067e
Sitoh7 <ha.wakhule@gmail.com>
2024-09-30 15:28:44 +0300

    use .map

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials.map(tutorial =>{
-    return tutorial.split(" ").map(word => word.charAt (0).toUpperCase()+ word.slice(1)).join(" ");
+const titleCased = () =>{
+  return tutorials.map(tutorial=>{
+    return tutorial.split(" ").map(string=>string.charAt(0).toUpperCase()+string.slice(1)).join(" ");
   } )
 }
